d881f03a313d1ddbd2238d47b711ae60062edeb9
junior-slv <dirceu.junior@edu.unifil.br>
2023-04-26 21:11:32 -0300

    initial files

--- server/dist/index.js
+++ server/dist/index.js
@@ -1,20 +1,12 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 Object.defineProperty(exports, "__esModule", { value: true });
-const sequelize_1 = require("./sequelize");
-function main() {
-    return __awaiter(this, void 0, void 0, function* () {
-        yield sequelize_1.default.authenticate();
-        console.log('Database connection successful!');
-    });
-}
-main();
+const express = require('express');
+const app = express();
+const port = 3000;
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
 //# sourceMappingURL=index.js.map
