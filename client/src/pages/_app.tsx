9a63a330c6359c76231305feb05730a5ec867cf6
p1nheiros <lucas.fernandes@edu.unifil.br>
2023-04-28 17:47:17 -0300

    Add a Show Password

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,6 +1,17 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { config, library } from '@fortawesome/fontawesome-svg-core';
+import '@fortawesome/fontawesome-svg-core/styles.css';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import '@/styles/globals.css';
+import { AppProps } from 'next/app';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+// Configuração inicial
+config.autoAddCss = false;
+
+// Adiciona os ícones que serão usados na aplicação
+library.add(faSearch);
+
+function App({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
 }
+
+export default App;
