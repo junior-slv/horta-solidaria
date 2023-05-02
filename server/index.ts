36039a778b0becf998caee70936dd17a9507d5bd
junior-slv <dirceu.junior@edu.unifil.br>
2023-05-02 16:13:27 -0300

    adiciona middleware e a api de adicição de doações

--- server/index.ts
+++ server/index.ts
@@ -1,8 +1,12 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3001
+const cors = require('cors')
 import db from './models'
 import { seedDoacao } from './seeders/seedDoacao'
+var corsOptions = {
+  origin: "http://localhost:3000"
+}
 
 const createUser = async () => {
   try {
@@ -14,7 +18,18 @@ const createUser = async () => {
     console.error(err);
   }
 }
-createUser();
+// createUser();
+
+//middleware
+app.use(cors(corsOptions))
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use((err:any, req:any, res:any, next:any) => {
+  console.error(err.stack);
+  res.status(500).send('Ocorreu um erro no servidor');
+});
+//
+
 
 //rotas
 const rotaDoacao = require('./routes/doacaoRotas')
