3783848d6ef2f407f4103d370c6803941c4bf359
junior-slv <dirceu.junior@edu.unifil.br>
2023-05-04 23:21:16 -0300

    componentização dos elementos de doação

--- client/src/pages/pessoas/principal.tsx
+++ client/src/pages/pessoas/principal.tsx
@@ -1,4 +1,4 @@
-import Sidebar from "@/components/Sidebar";
+import Sidebar from "@/components/sidebar/Sidebar";
 import React from "react";
 
 const Users = () => {
--- client/src/services/api.tsx
+++ client/src/services/api.tsx
@@ -6,20 +6,19 @@ export const instance = axios.create({
   baseURL: 'http://localhost:3001/api/'
 })
 
-export const signIn = (login: string | undefined, password: string | undefined) => {
-  instance.post("usuario/logar", { login, senha: password })
-    .then((res) => {
-      const auth = res.data.auth
-      const token = res.data.token
-      if (auth === true){
-        Router.push('/users');
-      }
-      // setCookie(undefined, 'horta.token', token, {
-      //   maxAge: 60 * 60 * 1, // 1 hora
-      //   secure: true, // Adiciona a opção secure ao cookie
-      // })
-    })
-    .catch((err) => {
-      console.error("Error" + err);
-    })
+export const signIn = (login: string | undefined, password: string | undefined ) => {
+  instance
+  .post("usuario/logar", {
+    login: `${login}`,
+    senha: `${password}`,
+  })
+  .then((res) => {
+    let auth = res.data.auth;
+    if (auth === true){
+      Router.push('/doacao/principal')
+    }
+  })
+  .catch((err) => {
+    console.error("Error" + err);
+  })
 }
