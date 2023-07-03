e688a16eb66167f4be092f778acdf8712344cffe
junior-slv <dirceu.junior@edu.unifil.br>
2023-07-02 23:41:00 -0300

    teste

--- server/dist/config/config.js
+++ server/dist/config/config.js
@@ -1,31 +1,32 @@
-"use strict";
 require('dotenv').config();
+
 const databaseUrl = process.env.DATABASE_URL;
 const databaseUsername = process.env.DATABASE_USERNAME;
 const databasePassword = process.env.DATABASE_PASSWORD;
 const databaseHost = process.env.DATABASE_HOST;
 const databaseDialect = process.env.DATABASE_DIALECT;
+
 module.exports = {
     development: {
-        username: databaseUsername,
-        password: databasePassword,
-        database: databaseUrl,
-        host: databaseHost,
-        dialect: databaseDialect,
+        username: "u756113144_junior",
+        password: "$2y$11$yMAOYs.L1Qp3Hn6P3kpZ0OblpwIxybYA6I4tQL70BH86qIU48Czkq",
+        database: "u756113144_hortaSolidaria",
+        host: "sql888.main-hosting.eu",
+        dialect: "mysql",
     },
     test: {
-        username: "root",
-        password: null,
-        database: "database_test",
-        host: "127.0.0.1",
+        username: "u756113144_junior",
+        password: "$2y$11$yMAOYs.L1Qp3Hn6P3kpZ0OblpwIxybYA6I4tQL70BH86qIU48Czkq",
+        database: "u756113144_hortaSolidaria",
+        host: "sql888.main-hosting.eu",
         dialect: "mysql",
     },
     production: {
-        username: "root",
-        password: null,
-        database: "database_production",
-        host: "127.0.0.1",
+        username: "u756113144_junior",
+        password: "$2y$11$yMAOYs.L1Qp3Hn6P3kpZ0OblpwIxybYA6I4tQL70BH86qIU48Czkq",
+        database: "u756113144_hortaSolidaria",
+        host: "sql888.main-hosting.eu",
         dialect: "mysql",
-    },
+    
+  },
 };
-//# sourceMappingURL=config.js.map
